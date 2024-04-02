850e66429e8a099074ff252dd5286915634b1dea
Pavel Davydov <Davydov.works@gmail.com>
2024-04-02 07:11:52 -0400

    add MiddleTask MyLibrary

--- src/main/java/Middle/MyLibrary/Main.java
+++ src/main/java/Middle/MyLibrary/Main.java
@@ -1,37 +1,43 @@
 package Middle.MyLibrary;
 
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        Books[] books = new Books[]{new Books("Война и Мир", "Лев Толстой"),
-                new Books("Мастер и Маргарита", "Михаил Булгаков"),
-                new Books("Жизнь в займы", "Эрих Мария Ремарк"),
-                new Books("Бесы", "Фёдор Михайлович Достоевский")};
+        ArrayList<Books> books = new ArrayList<>();
+        books.add(new Books("Война и Мир", "Лев Толстой"));
+        books.add(new Books("Мастер и Маргарита", "Михаил Булгаков"));
+        books.add(new Books("Жизнь в займы", "Эрих Мария Ремарк"));
+        books.add(new Books("Бесы", "Фёдор Михайлович Достоевский"));
 
 
-        Magazines[] magazines = new Magazines[]{new Magazines("Мода 2024"),
-                new Magazines("Рыбалка и Охота 2022"),
-                new Magazines("Лесные грибы"),
-                new Magazines("Вяжем в свободное время"),
-                new Magazines("Программирование для новичков")};
-
-        Stories[] stories = new Stories[]{new Stories("Наука и жизнь", "Антон Рыбаков"),
-                new Stories("Открытия и исследования", "Никита Поддубнов"),
-                new Stories("Что будет если?", "Наталья Долгих"),
-                new Stories("Я познаю компьютер", "Василий Черных"),
-                new Stories("От клавиатуры до монитора", "Стас Серых")};
+        ArrayList<Magazines> magazines = new ArrayList<>();
+        magazines.add(new Magazines("Мода 2024"));
+        magazines.add(new Magazines("Рыбалка и Охота 2022"));
+        magazines.add(new Magazines("Лесные грибы"));
+        magazines.add(new Magazines("Вяжем в свободное время"));
+        magazines.add(new Magazines("Программирование для новичков"));
 
 
-        Tales[] tales = new Tales[]{new Tales("Девочка и веер", "Марина Полякова"),
-                new Tales("Сказка о царе Салтане, о сыне его славном и могучем богатыре князе Гвидоне Салтановиче и о прекрасной царевне Лебеди", "Александр Пушкин"),
-                new Tales("Девочка Снегурочка", "Владимир Даль"),
-                new Tales("Ветер и Солнце", "Константин Ушинский"),
-                new Tales("Умнее всех", "Дмитрий Мамин-Сибиряк."),
-                new Tales("Рачья свадьба", "Алексей Николаевич Толстой")};
+        ArrayList<Stories> stories = new ArrayList<>();
+        stories.add(new Stories("Наука и жизнь", "Антон Рыбаков"));
+        stories.add(new Stories("Открытия и исследования", "Никита Поддубнов"));
+        stories.add(new Stories("Что будет если?", "Наталья Долгих"));
+        stories.add(new Stories("Я познаю компьютер", "Василий Черных"));
+        stories.add(new Stories("От клавиатуры до монитора", "Стас Серых"));
+
+
+        ArrayList<Tales> tales = new ArrayList<>();
+        tales.add(new Tales ("Девочка и веер", "Марина Полякова"));
+        tales.add(new Tales("Сказка о царе Салтане, о сыне его славном и могучем богатыре князе Гвидоне Салтановиче и о прекрасной царевне Лебеди", "Александр Пушкин"));
+        tales.add(new Tales("Девочка Снегурочка", "Владимир Даль"));
+        tales.add(new Tales("Ветер и Солнце", "Константин Ушинский"));
+        tales.add(new Tales("Умнее всех", "Дмитрий Мамин-Сибиряк."));
+        tales.add(new Tales("Рачья свадьба", "Алексей Николаевич Толстой"));
 
         System.out.println("Добро пожаловать в главное меню Библиотеки  " + " Всемирная библиотека им. Пушкина ");
         System.out.println("Пожалуйста выберите действия которые вы хотите совершить");
