5d1e98250e9fe5e2f3459b8fdd60bda7b3507bdb
Pavel Davydov <Davydov.works@gmail.com>
2024-04-04 05:45:48 -0400

    add Fridge

--- src/main/java/Middle/Refrigerator/Main.java
+++ src/main/java/Middle/Refrigerator/Main.java
@@ -1,4 +1,16 @@
 package Middle.Refrigerator;
 
 public class Main {
+    public static void main(String[] args) {
+        Fridge fridge = new Fridge(100);
+        Product bread = new Bread("Колбаса", 5);
+        Product fish = new Fish("Рыба", 10);
+        Product cheese = new Cheese("Сыр", 10);
+        Product meat = new Meat("Мясо", 25);
+        Product fruits = new  Fruits("фрукты", 15);
+
+        fridge.addProduct(new Fish("Семга", 5));
+
+        fridge.listOfProduct();
+    }
 }
