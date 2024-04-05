b284bf4aff6b99bcd4fd6ce0908ec671cdf6de91
Pavel Davydov <Davydov.works@gmail.com>
2024-04-05 04:54:04 -0400

    add workout

--- src/main/java/Middle/HomeWorkout/Main.java
+++ src/main/java/Middle/HomeWorkout/Main.java
@@ -1,7 +1,7 @@
 package Middle.HomeWorkout;
 
 public class Main {
-    public double caloriesBurnedRunning(Exercises exercises, int durationInMinutes) {
+    public double caloriesBurned(Exercises exercises, int durationInMinutes) {
         double caloriesPerMinute = exercises.calculateCaloriesPerMinute();
         return caloriesPerMinute * durationInMinutes;
     }
@@ -10,11 +10,12 @@ public class Main {
         Main calculator = new Main();
 
         Run run = new Run(85, 1.75);
-        double caloriesBurnedRunning = calculator.caloriesBurnedRunning(run, 30);
+        double caloriesBurnedRunning = calculator.caloriesBurned(run, 30);
         System.out.println("Калории сожженные бегом " + caloriesBurnedRunning);
 
         Jump jump = new Jump(85, 100);
-
+        double caloriesBurnedJump = calculator.caloriesBurned(jump, 15);
+        System.out.println("Калории сожженные прыжками " + caloriesBurnedJump);
 
     }
 }
